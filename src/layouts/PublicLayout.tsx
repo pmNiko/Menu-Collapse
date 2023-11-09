856ee02e9c8cbad31a7677e2362a64fe34ea8023
nikodev <martin.panebianco@smandes.gob.ar>
2023-11-09 13:06:02 -0300

    Home como outlet

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Footer, NavBar } from "../components";
-import { HomePage } from "../pages";
 
 export const PublicLayout = () => {
   return (
     <div>
       <NavBar />
 
-      <HomePage />
-      {/* <main>
+      <main>
         <Outlet />
-      </main> */}
+      </main>
       <Footer />
     </div>
   );
